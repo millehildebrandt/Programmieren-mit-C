3162d5e88b3856918b4a66e6497478d054404fec
Mille Hildebrandt <121965083+millehildebrandt@users.noreply.github.com>
2024-12-12 16:05:25 +0100

    Create (4) Bibliothek - stdio.h

---
+++ (4) Bibliothek - stdio.h
@@ -0,0 +1,14 @@
+//Datum:09.12.24
+/* Die Funktion printf ist (zumindest der reinen Lehre nach) dem Computer unbekannt.
+Der Compiler weiß also nicht, was er zu tun hat, wenn er den Befehl printf kompilieren soll.
+Daher muss zunächst eine Bibliothek eingeführt werden, in der dies "erklärt" wird.
+*/
+#include<stdio.h>
+
+main()
+{
+  printf(...)
+}
+/* Es gibt eine große Anzahl von Bibliotheken, die die unterschiedlichsten Funktionen erklären.
+Die Bibliothek, in der printf erklärt wird, heißt stdio.h (Abkürzung für STanDard Input Output)
+*/
